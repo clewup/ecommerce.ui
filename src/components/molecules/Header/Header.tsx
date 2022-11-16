92d51fb705d606bf0af0be652013c99d2b6d40b7
clew <lewis@clewup.co.uk>
2022-11-16 17:00:52 +0000

    Initial setup of router and layout.

---
+++ src/components/molecules/Header/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div>
+            <div>
+                header
+            </div>
+        </div>
+    )
+}
+export default Header;
---
+++ src/components/organisms/Layout/Layout.tsx
@@ -0,0 +1,17 @@
+import Header from "../../molecules/Header/Header";
+import Footer from "../../molecules/Footer/Footer";
+
+interface IProps {
+    children: JSX.Element;
+}
+
+const Layout: React.FC<IProps> = ({children}) => {
+    return (
+        <>
+            <Header/>
+            {children}
+            <Footer/>
+        </>
+    )
+}
+export default Layout;
