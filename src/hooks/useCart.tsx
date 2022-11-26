06b906be38ca1ca7169b9fe02c5188f4c6727594
clew <lewis@clewup.co.uk>
2022-11-26 22:17:48 +0000

    Cart enhancements.

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -11,58 +11,6 @@ const useCart = () => {
     localStorage.setItem("cart", JSON.stringify(cart));
   }, [cart]);
 
-  const addToCart = (product: IProduct, quantity: number, variant: string) => {
-    const addedCartItem = convertToCartItem(product, quantity, variant);
-
-    if (cart && addedCartItem) {
-      //Add item to the cart.
-      const updatedCart: ICart = {
-        cartItems: [...cart.cartItems, addedCartItem],
-        total: 0,
-      };
-
-      //Calculate total price.
-      let total = 0;
-      updatedCart.cartItems.map((cartItem: ICartItem) => {
-        total = total + cartItem.pricePerUnit * cartItem.quantity;
-        return null;
-      });
-      updatedCart.total = total;
-
-      setCart?.(updatedCart);
-    } else {
-      //Create the cart.
-      const createdCart = {
-        cartItems: [addedCartItem],
-        total: addedCartItem.pricePerUnit * addedCartItem.quantity,
-      };
-
-      setCart?.(createdCart);
-    }
-  };
-
-  const removeFromCart = (removedCartItem: ICartItem) => {
-    if (cart) {
-      //Remove item from the cart.
-      const updatedCartItems = cart.cartItems.filter(
-        (cartItem: ICartItem) => cartItem.id !== removedCartItem.id
-      );
-      const updatedCart = {
-        cartItems: updatedCartItems,
-        total: 0,
-      };
-
-      //Calculate total price.
-      let total = 0;
-      updatedCart.cartItems.map((cartItem: ICartItem) => {
-        total = total + cartItem.pricePerUnit * cartItem.quantity;
-      });
-      updatedCart.total = total;
-
-      setCart?.(updatedCart);
-    }
-  };
-
   const convertToCartItem = (
     product: IProduct,
     quantity: number,
@@ -78,6 +26,105 @@ const useCart = () => {
       discount: product.discount,
     };
     return cartItem;
+  };
+
+  const calculateTotal = (cart: ICart) => {
+    let totalledCart = cart;
+    let total = 0;
+
+    //Calculate total price.
+    totalledCart.cartItems.map((cartItem: ICartItem) => {
+      total = total + cartItem.pricePerUnit * cartItem.quantity;
+      return null;
+    });
+    totalledCart.total = total;
+    return totalledCart;
+  };
+
+  const addToCart = (product: IProduct, quantity: number, variant: string) => {
+    const addedCartItem = convertToCartItem(product, quantity, variant);
+
+    if (cart && addedCartItem) {
+      //Add item to the cart.
+
+      //If already in the basket, update the quantity.
+      if (cart.cartItems.some((cartItem) => cartItem.id === addedCartItem.id)) {
+        const currentCartItem: ICartItem = cart.cartItems.find(
+          (cartItem) => cartItem.id === addedCartItem.id
+        )!;
+        currentCartItem.quantity =
+          currentCartItem.quantity + addedCartItem.quantity;
+
+        const updatedCart: ICart = {
+          cartItems: [
+            ...cart.cartItems.filter(
+              (cartItem) => cartItem.id !== addedCartItem.id
+            ),
+            currentCartItem,
+          ],
+          total: 0,
+        };
+        const totalledCart = calculateTotal(updatedCart);
+        setCart?.(totalledCart);
+      }
+      //If not already in the basket, add to the array,
+      else {
+        const updatedCart: ICart = {
+          cartItems: [...cart.cartItems, addedCartItem],
+          total: 0,
+        };
+        const totalledCart = calculateTotal(updatedCart);
+        setCart?.(totalledCart);
+      }
+    } else {
+      //Create the cart.
+      const createdCart = {
+        cartItems: [addedCartItem],
+        total: addedCartItem.pricePerUnit * addedCartItem.quantity,
+      };
+
+      setCart?.(createdCart);
+    }
+  };
+
+  const removeFromCart = (removedCartItem: ICartItem) => {
+    if (cart) {
+      //If quantity is more than 1, update the quantity.
+      if (
+        cart.cartItems.find((cartItem) => cartItem.id === removedCartItem.id)!
+          .quantity > 1
+      ) {
+        const currentCartItem: ICartItem = cart.cartItems.find(
+          (cartItem) => cartItem.id === removedCartItem.id
+        )!;
+        currentCartItem.quantity = currentCartItem.quantity - 1;
+
+        const updatedCart: ICart = {
+          cartItems: [
+            ...cart.cartItems.filter(
+              (cartItem) => cartItem.id !== removedCartItem.id
+            ),
+            currentCartItem,
+          ],
+          total: 0,
+        };
+        const totalledCart = calculateTotal(updatedCart);
+        setCart?.(totalledCart);
+      }
+      // If quantity is 1, remove it from the array.
+      else {
+        const updatedCartItems = cart.cartItems.filter(
+          (cartItem: ICartItem) => cartItem.id !== removedCartItem.id
+        );
+        const updatedCart = {
+          cartItems: updatedCartItems,
+          total: 0,
+        };
+
+        const totalledCart = calculateTotal(updatedCart);
+        setCart?.(totalledCart);
+      }
+    }
   };
 
   return { cart, addToCart, removeFromCart };
