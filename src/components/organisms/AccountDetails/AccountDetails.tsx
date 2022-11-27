4201b564dd47ae6d2e7063faae36dc0be6bb4213
clew <lewis@clewup.co.uk>
2022-11-27 21:38:12 +0000

    Enabled prepopulation of account details form using MUI components.

--- src/components/organisms/AccountDetails/AccountDetails.tsx
+++ src/components/organisms/AccountDetails/AccountDetails.tsx
@@ -7,7 +7,7 @@ import Input from "../../atoms/Input/Input";
 import Subheading from "../../atoms/Subheading/Subheading";
 
 interface IProps {
-  user?: IUser;
+  user: IUser;
   isEditing: boolean;
 }
 
@@ -15,7 +15,7 @@ const AccountDetails: React.FC<IProps> = ({ user, isEditing }) => {
   const updateUser = (values: FormikValues) => {};
 
   return (
-    <Formik initialValues={user!} onSubmit={(values) => updateUser(values)}>
+    <Formik initialValues={user} onSubmit={(values) => updateUser(values)}>
       {(formik) => {
         return (
           <div id={"account-details"}>
@@ -27,18 +27,24 @@ const AccountDetails: React.FC<IProps> = ({ user, isEditing }) => {
                   component={Input}
                   label={"First Name"}
                   disabled={!isEditing}
+                  onChange={formik.handleChange}
+                  value={formik.values.firstName}
                 />
                 <Field
                   name={"lastName"}
                   component={Input}
                   label={"Last Name"}
                   disabled={!isEditing}
+                  onChange={formik.handleChange}
+                  value={formik.values.lastName}
                 />
                 <Field
                   name={"email"}
                   component={Input}
                   label={"Email"}
                   disabled={!isEditing}
+                  onChange={formik.handleChange}
+                  value={formik.values.email}
                 />
               </div>
 
@@ -49,42 +55,56 @@ const AccountDetails: React.FC<IProps> = ({ user, isEditing }) => {
                   component={Input}
                   label={"Line One"}
                   disabled={!isEditing}
+                  onChange={formik.handleChange}
+                  value={formik.values.lineOne}
                 />
                 <Field
                   name={"lineTwo"}
                   component={Input}
                   label={"Line Two"}
                   disabled={!isEditing}
+                  onChange={formik.handleChange}
+                  value={formik.values.lineTwo}
                 />
                 <Field
                   name={"lineThree"}
                   component={Input}
                   label={"Line Three"}
                   disabled={!isEditing}
+                  onChange={formik.handleChange}
+                  value={formik.values.lineThree}
                 />
                 <Field
                   name={"postcode"}
                   component={Input}
                   label={"Postcode"}
                   disabled={!isEditing}
+                  onChange={formik.handleChange}
+                  value={formik.values.postcode}
                 />
                 <Field
                   name={"city"}
                   component={Input}
                   label={"City"}
                   disabled={!isEditing}
+                  onChange={formik.handleChange}
+                  value={formik.values.city}
                 />
                 <Field
                   name={"county"}
                   component={Input}
                   label={"County"}
                   disabled={!isEditing}
+                  onChange={formik.handleChange}
+                  value={formik.values.county}
                 />
                 <Field
                   name={"country"}
                   component={Input}
                   label={"Country"}
                   disabled={!isEditing}
+                  onChange={formik.handleChange}
+                  value={formik.values.country}
                 />
               </div>
             </Form>
