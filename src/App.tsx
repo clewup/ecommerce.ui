802935184fd277db737bc04d69554fecce41f184
clew <lewis@clewup.co.uk>
2022-12-09 14:09:31 +0000

    Added default axios header for authorization.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import Layout from "./components/organisms/Layout/Layout";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home/Home";
@@ -10,8 +10,14 @@ import Account from "./components/pages/Account/Account";
 import Register from "./components/pages/Register/Register";
 import Product from "./components/pages/Product/Product";
 import Checkout from "./components/pages/Checkout/Checkout";
+import { AuthContext } from "./contexts/Auth";
+import axios from "axios";
 
 const App = () => {
+  const { accessToken } = useContext(AuthContext);
+
+  axios.defaults.headers.common["Authorization"] = "Bearer " + accessToken;
+
   return (
     <section id={"ecommerce-root"}>
       <Layout>
