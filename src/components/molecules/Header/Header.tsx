c8bd0289c09109fb7f0034d6f9ef035c705d1a55
clew <lewis@clewup.co.uk>
2022-12-11 00:04:31 +0000

    Created header dropdown for store categories.

--- src/components/molecules/Header/Header.tsx
+++ src/components/molecules/Header/Header.tsx
@@ -1,42 +1,171 @@
 import "./header.scss";
-import { Link } from "react-router-dom";
-import { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import React, { useContext } from "react";
 import { AuthContext } from "../../../contexts/Auth";
 import { roles } from "../../../enums/roles";
+import {
+  Button,
+  ClickAwayListener,
+  Grow,
+  Menu,
+  MenuItem,
+  MenuList,
+  Paper,
+  Popper,
+} from "@mui/material";
 
 const Header = () => {
   const { isAuthenticated, role } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef<HTMLButtonElement>(null);
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event: Event | React.SyntheticEvent) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event: React.KeyboardEvent) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    } else if (event.key === "Escape") {
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current!.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
 
   return (
     <div id={"header"}>
       <div className={"header-group"}>
-        <Link to={"/"}>
-          <p>Home</p>
-        </Link>
-        <Link to={"store"}>
-          <p>Store</p>
-        </Link>
-        <Link to={"trending"}>
-          <p>Trending</p>
-        </Link>
+        <Button onClick={() => navigate("/")} sx={{ color: "white" }}>
+          Home
+        </Button>
+
+        <Button
+          ref={anchorRef}
+          id="composition-button"
+          aria-controls={open ? "composition-menu" : undefined}
+          aria-expanded={open ? "true" : undefined}
+          aria-haspopup="true"
+          onClick={handleToggle}
+          sx={{ color: "white" }}
+        >
+          Store
+        </Button>
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          placement="bottom-start"
+          transition
+          disablePortal
+        >
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{
+                transformOrigin:
+                  placement === "bottom-start" ? "left top" : "left bottom",
+              }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList
+                    autoFocusItem={open}
+                    id="composition-menu"
+                    aria-labelledby="composition-button"
+                    onKeyDown={handleListKeyDown}
+                  >
+                    <MenuItem
+                      onClick={(e) => {
+                        handleClose(e);
+                        navigate("store");
+                      }}
+                    >
+                      All Products
+                    </MenuItem>
+                    <MenuItem
+                      onClick={(e) => {
+                        handleClose(e);
+                        navigate("store/laptops");
+                      }}
+                    >
+                      Laptops
+                    </MenuItem>
+                    <MenuItem
+                      onClick={(e) => {
+                        handleClose(e);
+                        navigate("store/mobile-phones");
+                      }}
+                    >
+                      Mobile Phones
+                    </MenuItem>
+                    <MenuItem
+                      onClick={(e) => {
+                        handleClose(e);
+                        navigate("store/smart-watches");
+                      }}
+                    >
+                      Smart Watches
+                    </MenuItem>
+                    <MenuItem
+                      onClick={(e) => {
+                        handleClose(e);
+                        navigate("store/tablets");
+                      }}
+                    >
+                      Tablets
+                    </MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+
+        <Button onClick={() => navigate("trending")} sx={{ color: "white" }}>
+          Trending
+        </Button>
       </div>
       <div className={"header-group"}>
         {!isAuthenticated ? (
-          <Link to={"login"}>
-            <p>Login</p>
-          </Link>
+          <Button onClick={() => navigate("login")} sx={{ color: "white" }}>
+            Login
+          </Button>
         ) : (
-          <Link to={"account"}>
-            <p>Account</p>
-          </Link>
+          <Button onClick={() => navigate("account")} sx={{ color: "white" }}>
+            Account
+          </Button>
         )}
-        <Link to={"cart"}>
-          <p>Basket</p>
-        </Link>
+        <Button onClick={() => navigate("cart")} sx={{ color: "white" }}>
+          Cart
+        </Button>
+
         {role && (role === roles.DEVELOPER || role === roles.EMPLOYEE) && (
-          <Link to={"admin"}>
-            <p>Admin</p>
-          </Link>
+          <Button onClick={() => navigate("admin")} sx={{ color: "white" }}>
+            Admin
+          </Button>
         )}
       </div>
     </div>
--- src/components/pages/Store/Store.tsx
+++ src/components/pages/Store/Store.tsx
@@ -6,14 +6,21 @@ import Wrapper from "../../atoms/Wrapper/Wrapper";
 import { useParams } from "react-router-dom";
 import { useContext, useEffect } from "react";
 import { ProductContext } from "../../../contexts/Product";
+import useProductFilter from "../../../hooks/useProductFilter";
 
 const Store = () => {
   const { category } = useParams();
   const { setCategoryQuery } = useContext(ProductContext);
+  const { categories } = useProductFilter();
 
   useEffect(() => {
     if (category) {
-      setCategoryQuery(category);
+      const matchingCategory = categories.find(
+        (c) => c.replace(/\s/g, "-").toLowerCase() === category.toLowerCase()
+      );
+      if (matchingCategory) {
+        setCategoryQuery(matchingCategory);
+      }
     }
   });
 
